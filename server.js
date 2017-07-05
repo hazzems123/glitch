af5b590a1a279216a6873d1d026dc9e5615d3038
System <none>
2017-07-05 21:42:28 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const http = require('http')
 
-http.createServer(function (req, res) {
-  console.log(`received request: ${req.method} ${req.url}`)
+http.createServer(handleRequest).listen(3000)
 
-  res.write('ok')
-}).listen(3000)
+function handleRequest (request, response) {
+  
+  // read out the request body if present
+  let payload = ''
+  if (request.method == 'POST') request.on('data', (data) => payload += data );
+  
+  // log request method & URL as well as the body if present
+  console.log(`${request.method} ${request.url}`)
+  request.on('end', function () {
+    if (payload) console.log(payload)
+    
+    // return a response
+    response.end('ok')
+  })
+}
