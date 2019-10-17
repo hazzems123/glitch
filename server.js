646ea7d808568d092fee550a2f9c7e6f95a9506f
Glitch (node-server-example) <none>
2019-10-17 01:39:13 +0000

    📟☔️ Checkpoint ./server.js:1427966/3957

--- server.js
+++ server.js
@@ -1,20 +1,69 @@
-const http = require('http')
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-http.createServer(handleRequest).listen(3000)
+http.createServer(function (request, response) {
+            console.log('request ', request.url);
+            const {headers, url, method } = request;
+         //reading the body request from the server
+        let body = [];
+          request.on('error', (err) => {
+                      console.error(err);
+                    }).on('data', (chunk) => {
+                                body.push(chunk);
+                              }).on('end', () => {
+                                          body = Buffer.concat(body).toString();
+                                          // BEGINNING OF NEW STUFF
+          })
+                                       response.on('error', (err) => {
+                                      console.error(err);
+                                      });
 
-function handleRequest (request, response) {
-  
-  // log request method & URL
-  console.log(`${request.method} ${request.url}`)
-  
-  // for GET (and other non-POST) requests show "ok" and stop here
-  if (request.method !== 'POST') return response.end('ok')
-  
-  // for POST requests, read out the request body, log it, then show "ok" as response
-  let payload = ''
-  request.on('data', (data) => payload += data );
-  request.on('end', () => {
-    console.log(payload)
-    response.end('ok')
-  })
-}
+
+            var filePath = '.' + request.url;
+            if (filePath == './') {
+                            filePath = './index.html';
+                        }
+
+            var extname = String(path.extname(filePath)).toLowerCase();
+            var mimeTypes = {
+               
+            '.html': 'text/html',
+                            '.js': 'text/javascript',
+                            '.css': 'text/css',
+                            '.json': 'application/json',
+                            '.png': 'image/png',
+                            '.jpg': 'image/jpg',
+                            '.gif': 'image/gif',
+                            '.svg': 'image/svg+xml',
+                            '.wav': 'audio/wav',
+                            '.mp4': 'video/mp4',
+                            '.woff': 'application/font-woff',
+                            '.ttf': 'application/font-ttf',
+                            '.eot': 'application/vnd.ms-fontobject',
+                            '.otf': 'application/font-otf',
+                            '.wasm': 'application/wasm'
+                        };
+
+            var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+            fs.readFile(filePath, function(error, content) {
+              if (error) {
+                if(error.code == 'ENOENT') {
+                    fs.readFile('./404.html', function(error, content) {
+                     response.writeHead(404, { 'Content-Type': contentType });
+                     response.end(content, 'utf-8');
+                     })
+                     }
+ else {
+                     response.writeHead(500);
+                     response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                      }
+                      }
+             else {
+                 response.writeHead(200, { 'Content-Type': contentType });
+                 response.end(content, 'utf-8');
+                                            }
+                        })
+
+}).listen(8125)
