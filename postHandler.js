907c9bccaabb983fd55c9fe35aa392b987e189c7
Glitch (node-server-example) <none>
2019-10-18 19:55:00 +0000

    👟👦 Checkpoint ./postHandler.js:1427966/459

--- postHandler.js
+++ postHandler.js
@@ -8,7 +8,16 @@ if (req.method === 'POST') {
       console.log ('the received chunk :'+chunk)
       const mongourl = "mongodb+srv://hazzems:iloverony123!@cluster0-atcd6.mongodb.net/test?retryWrites=true&w=majority"
         const db = await MongoClient.connect(mongourl,{ useNewUrlParser: true, useUnifiedTopology: true }, (err,client,chunk)=> {
-          console.log ("this is the client :"+ Object.keys(client)
+          if (!err){
+          console.log ("this is the client :"+ Object.keys(client))
+            db.collection('orders').insertOne({
+        size: chunk.size,
+        qty: chunk.qty
+    });
+          }
+          else {
+            console.log ('error connecting to the db')
+          }
        
  })
       body = JSON.stringify(chunk)
