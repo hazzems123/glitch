985046711059b8bb6c3c20fd46aebfac20197b2a
Glitch (node-server-example) <none>
2019-10-18 20:06:01 +0000

    🌦🏮 Checkpoint ./postHandler.js:1427966/534

--- postHandler.js
+++ postHandler.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient;
-
 let posthandler = (req,res,cb)=>{
 if (req.method === 'POST') {
     let body
     req.on('data', async (chunk) => {
-      chunk = JSON.parse(chunk)
-      console.log ('the received chunk :'+chunk)
+      chunk = await JSON.parse(chunk);
+      console.log ('the received chunk :'+chunk.size)
       const mongourl = "mongodb+srv://hazzems:iloverony123!@cluster0-atcd6.mongodb.net/test?retryWrites=true&w=majority"
-        const db = await MongoClient.connect(mongourl,{ useNewUrlParser: true, useUnifiedTopology: true }, (err,client,chunk)=> {
+        const db = await MongoClient.connect(mongourl,{ useNewUrlParser: true, useUnifiedTopology: true }, (err,client)=> {
           if (!err){
-          console.log ("this is the client :"+ Object.keys(client))
+            const db = client.db('waistnshape')
+                                
             db.collection('orders').insertOne({
         size: chunk.size,
         qty: chunk.qty
