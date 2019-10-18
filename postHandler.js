2b33272c75eab9a18b672e5e53433a3ddacb370c
Glitch (node-server-example) <none>
2019-10-18 23:53:12 +0000

    🍶💁 Checkpoint ./postHandler.js:1427966/293

--- postHandler.js
+++ postHandler.js
@@ -4,24 +4,25 @@ if (req.method === 'POST') {
     let body
     req.on('data', async (chunk) => {
       chunk = JSON.parse(chunk);
-      console.log ('the received chunk :'+chunk.size)
+      console.log ('the received chunk :'+chunk[0].size)
       
-
+      var orderno = Math.floor(100000 + Math.random() * 900000);
       const mongourl = "mongodb+srv://hazzems:iloverony123!@cluster0-atcd6.mongodb.net/test?retryWrites=true&w=majority"
         const db = await MongoClient.connect(mongourl,{ useNewUrlParser: true, useUnifiedTopology: true }, (err,client)=> {
           if (!err){
-           var orderno = Math.floor(100000 + Math.random() * 900000);
             const db = client.db('waistnshape')
         //create an order number for the customer 
+        for (var i = 0 ; i < chunk.lenght;i++){
         db.collection('orders').insertOne({
         orderno : orderno,
-        name : chunk.name,
-        email : chunk.email,
-        mobile : chunk.mobile,
-        address : chunk.address,
-        size: chunk.size,
-        qty: chunk.qty
+        name : chunk[i].name,
+        email : chunk[i].email,
+        mobile : chunk[i].mobile,
+        address : chunk[i].address,
+        size: chunk[i].size,
+        qty: chunk[i].qty
     });
+        }
           }
           else {
             console.log ('error connecting to the db')
